04d512e0482602f9cfb6300b834943b924eaa306
Георгий <dfffgfgf@yandex.ru>
2021-08-10 13:31:57 +0300

    add permission

--- backend/foodgram/recipes/permissions.py
+++ backend/foodgram/recipes/permissions.py
@@ -1,6 +1,14 @@
 from rest_framework import permissions
 
 
-class IsAuthor(permissions.BasePermission):
+class RecipePermissions(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return request.user.is_authenticated
+        return request.method in permissions.SAFE_METHODS
+
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.method in ['PUT', 'DELETE']:
+            return obj.user == request.user
