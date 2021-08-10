66de0d3c35cd437e3f59c586ee73956f9014e1a9
Георгий <dfffgfgf@yandex.ru>
2021-08-10 14:50:17 +0300

    fix flake8

--- backend/foodgram/recipes/urls.py
+++ backend/foodgram/recipes/urls.py
@@ -18,11 +18,25 @@ recipes_api.register('recipes', RecipeViewSet, basename='recipes')
 
 
 urlpatterns = [
-    path('recipes/download_shopping_cart/', ShoppingCartDownload.as_view(), name='download_shopping_cart'),
+    path('recipes/download_shopping_cart/',
+         ShoppingCartDownload.as_view(),
+         name='download_shopping_cart'
+         ),
     path('', include(recipes_api.urls)),
-    path('recipes/<int:recipe_id>/favorite/', FavoriteRecipeView.as_view(), name='favorite_recipe'),
-    path('recipes/<int:recipe_id>/shopping_cart/', ShoppingCartView.as_view(), name='shopping_cart'),
-    path('users/subscriptions/', UserSubscribeListView.as_view(), name='subscriptions'),
-    path('users/<int:author_id>/subscribe/', SubscribeView.as_view(), name='create_destroy_subs'),
-
+    path('recipes/<int:recipe_id>/favorite/',
+         FavoriteRecipeView.as_view(),
+         name='favorite_recipe'
+         ),
+    path('recipes/<int:recipe_id>/shopping_cart/',
+         ShoppingCartView.as_view(),
+         name='shopping_cart'
+         ),
+    path('users/subscriptions/',
+         UserSubscribeListView.as_view(),
+         name='subscriptions'
+         ),
+    path('users/<int:author_id>/subscribe/',
+         SubscribeView.as_view(),
+         name='create_destroy_subs'
+         ),
 ]
