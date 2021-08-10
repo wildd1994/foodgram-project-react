97e7de8152245d846a1026c861eca005e11f476c
Георгий <dfffgfgf@yandex.ru>
2021-08-10 13:31:08 +0300

    fix tags filter

--- backend/foodgram/recipes/filters.py
+++ backend/foodgram/recipes/filters.py
@@ -1,22 +1,42 @@
 from django_filters import rest_framework as filters
-from .models import Recipe
+from .models import Recipe, Tag
 
 
 class CustomFilter(filters.FilterSet):
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.BooleanFilter(method='get_favorite')
-    is_in_shopping_cart = filters.BooleanFilter(method='get_shopping_cart')
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        queryset=Tag.objects.all(),
+        to_field_name='slug'
+    )
+    is_favorited = filters.BooleanFilter(
+        method='get_favorite'
+    )
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='get_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
-        fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
+        fields = ('tags',
+                  'author',
+                  'is_favorited',
+                  'is_in_shopping_cart'
+                  )
 
     def get_favorite(self, queryset, name, value):
         if value:
-            return Recipe.objects.filter(favorited_recipe__user=self.request.user)
-        return Recipe.objects.all().exclude(favorited_recipe__user=self.request.user)
+            return Recipe.objects.filter(
+                favorited_recipe__user=self.request.user
+            )
+        return Recipe.objects.all().exclude(
+            favorited_recipe__user=self.request.user
+        )
 
     def get_shopping_cart(self, queryset, name, value):
         if value:
-            return Recipe.objects.filter(recipe_in_shopping_cart__user=self.request.user)
-        return Recipe.objects.all().exclude(recipe_in_shopping_cart__user=self.request.user)
+            return Recipe.objects.filter(
+                recipe_in_shopping_cart__user=self.request.user
+            )
+        return Recipe.objects.all().exclude(
+            recipe_in_shopping_cart__user=self.request.user
+        )
