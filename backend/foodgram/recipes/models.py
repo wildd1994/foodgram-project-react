0b5d94453ae6e757d60ec178439083448f8e3e16
Георгий <dfffgfgf@yandex.ru>
2021-08-13 12:26:43 +0300

    add verbose names

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -17,11 +17,17 @@ def is_hex(value):
 class Ingredient(models.Model):
     name = models.CharField(
         max_length=200,
-        blank=False
+        blank=False,
+        verbose_name='Название ингредиента'
     )
     measurement_unit = models.CharField(
-        max_length=200
+        max_length=200,
+        verbose_name='Еденица измерения'
     )
+
+    class Meta:
+        verbose_name = 'Интгредиент'
+        verbose_name_plural = 'Ингредиенты'
 
     def __str__(self):
         return f'{self.name}, {self.measurement_unit}'
@@ -30,21 +36,28 @@ class Ingredient(models.Model):
 class Tag(models.Model):
     name = models.CharField(
         max_length=200,
-        blank=False
+        blank=False,
+        verbose_name='Название тега'
     )
     color = models.CharField(
         null=True,
         blank=True,
         max_length=7,
-        validators=[is_hex]
+        validators=[is_hex],
+        verbose_name='Цвет'
     )
     slug = models.SlugField(
         null=True,
         max_length=200,
-        blank=True
+        blank=True,
+        verbose_name='Уникальный слаг'
     )
 
     REQUIRED_FIELDS = ['name']
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
 
     def __str__(self):
         return f'{self.name}'
@@ -53,31 +66,43 @@ class Tag(models.Model):
 class Recipe(models.Model):
     author = models.ForeignKey(
         User,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name='Автор рецепта'
     )
     name = models.CharField(
-        max_length=200
+        max_length=200,
+        verbose_name='Название рецепта'
     )
     ingredients = models.ManyToManyField(
         Ingredient,
-        through='IngredientInRecipe'
+        through='IngredientInRecipe',
+        verbose_name='Ингредиенты'
     )
     tags = models.ManyToManyField(
         Tag,
-        related_name='tags'
+        related_name='tags',
+        verbose_name='Теги для рецепта'
     )
-    text = models.TextField()
-    cooking_time = models.PositiveIntegerField()
+    text = models.TextField(
+        verbose_name='Способ приготовления'
+    )
+    cooking_time = models.PositiveIntegerField(
+        verbose_name='Время приготовления'
+    )
     image = models.ImageField(
-        upload_to=''
+        upload_to='',
+        verbose_name='Изображение'
     )
     pub_date = models.DateTimeField(
         auto_now_add=True,
-        db_index=True
+        db_index=True,
+        verbose_name='Дата публикации'
     )
 
     class Meta:
         ordering = ('pub_date',)
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
 
     def __str__(self):
         return f'{self.name}'
@@ -87,26 +112,32 @@ class IngredientInRecipe(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='recipe'
+        related_name='recipe',
+        verbose_name='Рецепт'
     )
     ingredient = models.ForeignKey(
         Ingredient,
         on_delete=models.CASCADE,
-        related_name='ingredient'
+        related_name='ingredient',
+        verbose_name='Ингредиент'
     )
-    amount = models.PositiveIntegerField()
+    amount = models.PositiveIntegerField(
+        verbose_name='Количество ингредиента'
+    )
 
 
 class FavoriteRecipe(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='favorited_recipe'
+        related_name='favorited_recipe',
+        verbose_name='Избранный рецепт'
     )
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='users_favorited'
+        related_name='users_favorited',
+        verbose_name='Пользователь'
     )
 
     class Meta:
@@ -116,18 +147,22 @@ class FavoriteRecipe(models.Model):
                 name='unique_favorite_list'
             ),
         ]
+        verbose_name = 'Ингредиент в рецепте'
+        verbose_name_plural = 'Ингредиенты в рецептах'
 
 
 class ShoppingCart(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='recipe_in_shopping_cart'
+        related_name='recipe_in_shopping_cart',
+        verbose_name='Рецепт в корзине'
     )
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='user_shopping_cart'
+        related_name='user_shopping_cart',
+        verbose_name='Пользователь'
     )
 
     class Meta:
@@ -137,18 +172,22 @@ class ShoppingCart(models.Model):
                 name='unique_shopping_cart_list'
             ),
         ]
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзины'
 
 
 class Subscribe(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='follower'
+        related_name='follower',
+        verbose_name='Подписчик'
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='following'
+        related_name='following',
+        verbose_name='Автор'
     )
 
     def __str__(self):
@@ -161,3 +200,5 @@ class Subscribe(models.Model):
                 name='unique_subscribe_list'
             ),
         ]
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -3,11 +3,24 @@ from django.contrib.auth.models import AbstractUser
 
 
 class FoodgramUser(AbstractUser):
-    email = models.EmailField(max_length=254, unique=True)
-    username = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='email'
+    )
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name='Логин'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия'
+    )
 
     class Meta:
         ordering = ('id',)
