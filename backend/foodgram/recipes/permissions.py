177e8b05b90b14159856e54592df6cee266f820e
Георгий <dfffgfgf@yandex.ru>
2021-08-19 16:50:27 +0300

    hotfix

--- backend/foodgram/recipes/permissions.py
+++ backend/foodgram/recipes/permissions.py
@@ -11,4 +11,4 @@ class RecipePermissions(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         if request.method in ['PUT', 'DELETE']:
-            return obj.user == request.user
+            return obj.author == request.user
